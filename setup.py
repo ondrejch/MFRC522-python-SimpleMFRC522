9fc5240ee421d3a5e4538afb0e2108bdd40bd24a
Dennis-89 <straub.dennis1@web.de>
2024-02-15 17:39:43 +0100

    Add Pathlib

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 import setuptools
+from pathlib import Path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+README_FILE = Path().resolve() / "README.md"
 
-setuptools.setup(
-    name="mfrc522",
-    version="1.0.0",
-    author="Pi My Life Up / Dennis89",
-    author_email="straub.dennis1@web.de",
-    description="A library to integrate the MFRC522 RFID readers with the Raspberry Pi",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Dennis-89/MFRC522-python-SimpleMFRC522",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'gpiozero',
-        'spidev'
+
+def main():
+    long_description = README_FILE.read_text()
+
+    setuptools.setup(
+        name="mfrc522",
+        version="1.0.0",
+        author="Pi My Life Up / Dennis89",
+        author_email="straub.dennis1@web.de",
+        description="A library to integrate the MFRC522 RFID readers with the Raspberry Pi",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/Dennis-89/MFRC522-python-SimpleMFRC522",
+        packages=setuptools.find_packages(),
+        install_requires=["gpiozero", "spidev"],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+            "Operating System :: POSIX :: Linux",
+            "Topic :: System :: Hardware",
         ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Operating System :: POSIX :: Linux",
-        'Topic :: System :: Hardware',
-    ],
-)
+    )
+
+
+if __name__ == "__main__":
+    main()
